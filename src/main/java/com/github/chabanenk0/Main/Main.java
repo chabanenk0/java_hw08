169650def9a7cf5604be28c32ee727e24c3da607
Dmitry Chabanenko <chdn6026@mail.ru>
2017-01-25 23:49:29 +0200

    Initial commit

---
+++ src/main/java/com/github/chabanenk0/Main/Main.java
@@ -0,0 +1,10 @@
+package com.github.chabanenk0.Main;
+
+/**
+ * Created by dmitry on 25.01.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/test/java/com/github/chabanenk0/Main/MainTest.java
@@ -0,0 +1,18 @@
+package com.github.chabanenk0.Main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by dmitry on 25.01.17.
+ */
+public class MainTest
+{
+    @Test
+    public void mainTest()
+    {
+        Main main = new Main();
+        assertTrue(true);
+    }
+}
